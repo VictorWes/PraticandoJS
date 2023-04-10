1b207ebdd5e87bf190fc760121030af33536e77b
Victor Wesley <victor.infowb@gmail.com>
2023-04-10 18:53:25 -0300

    Refatorando Swagger pt2

--- src/routes/routes.swagger.cjs
+++ src/routes/routes.swagger.cjs
@@ -1,9 +1,9 @@
-const router = require("express").Router();
+const routerSwagger = require("express").Router();
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("../swagger.json");
 
-router.use("/", swaggerUi.serve);
-router.get("/", swaggerUi.setup(swaggerDocument));
+routerSwagger.use("/", swaggerUi.serve);
+routerSwagger.get("/", swaggerUi.setup(swaggerDocument));
 
-module.exports = router;
+module.exports = routerSwagger;
